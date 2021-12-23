71f194bb7da64850f0bd3b65918bc6f8606179f3
Muhammad Attalariq <attalariq002@gmail.com>
2021-12-23 22:20:47 +0700

    add throws

--- Tubes_PBO_2021/src/DRIVER/KoneksiDB.java
+++ Tubes_PBO_2021/src/DRIVER/KoneksiDB.java
@@ -8,6 +8,8 @@ package DRIVER;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,7 +18,7 @@ import java.sql.SQLException;
 public class KoneksiDB {
     private static Connection koneksi;
     
-    public static Connection getKoneksi() {
+    public static Connection getKoneksi() throws SQLException {
         if (koneksi == null) {
             try {
                 String url = "jdbc:mysql://localhost:3306/inventory";
@@ -25,7 +27,8 @@ public class KoneksiDB {
                 
                 koneksi = DriverManager.getConnection(url, user, pass);
                 System.out.println("Koneksi Berhasil...");
-            } catch(SQLException e) {
+            } catch(SQLException ex) {
+                Logger.getLogger(KoneksiDB.class.getName()).log(Level.SEVERE, null, ex);
                 System.out.println("Koneksi Gagal");
             }
         }
